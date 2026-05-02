6b8be7b9349fe322834f460dbf9cff2b901e3981
thutrang0410 <trhieuforword@gmail.com>
2026-05-02 10:12:07 +0700

    Update tai-files.sh

--- tai-files.sh
+++ tai-files.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
 APK_NAME="Phicomm-R1e.apk"
-APK_URL="https://github.com/trunghieu1604/r1-ai/releases/download/v1.0/$APK_NAME"
+APK_URL="https://github.com/thutrang0410/vietbot/releases/download/r1/$APK_NAME"
 APK_PATH="$HOME/$APK_NAME"
 
 DLNA_APK_NAME="auto-dlna.apk"
 DLNA_APK_LOCAL_PATH="$HOME/$DLNA_APK_NAME"
-DLNA_APK_URL="https://github.com/trunghieu1604/r1-ai/releases/download/v1.0/$DLNA_APK_NAME"
+DLNA_APK_URL="https://github.com/thutrang0410/vietbot/releases/download/r1/$DLNA_APK_NAME"
 
 UNISOUND_APK_NAME="uni-sound.apk"
 UNISOUND_APK_LOCAL_PATH="$HOME/$UNISOUND_APK_NAME"
-UNISOUND_APK_URL="https://github.com/trunghieu1604/r1-ai/releases/download/v1.0/$UNISOUND_APK_NAME"
+UNISOUND_APK_URL="https://github.com/thutrang0410/vietbot/releases/download/r1/$UNISOUND_APK_NAME"
 prepare_apk() {
     local apk_path="$1"
     local apk_url="$2"
